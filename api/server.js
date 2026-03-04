1c4aa6ced82b0d5682b21e236f6d7e6d03e1cdd1
Saraqch <quispechoquesara@gmail.com>
2026-03-04 06:45:36 -0400

    Configurar deploy para Render

--- api/server.js
+++ api/server.js
@@ -7,12 +7,29 @@ const fileUpload = require("express-fileupload");
 const slugify = require("slugify");
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://saraqch.github.io'
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido por CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(fileUpload());
 
 const GRAV_PAGES = path.join(__dirname, "../grav/user/pages/04.noticias");
 const ASSETS_PATH = path.join(__dirname, "../frontend/public/assets/images");
+
+console.log("Pages Path:", GRAV_PAGES);
+console.log("Assets Path:", ASSETS_PATH);
 
 // Asegurar que la carpeta de assets existe
 if (!fs.existsSync(ASSETS_PATH)) {
